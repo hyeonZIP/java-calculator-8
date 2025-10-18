45c975178fff396dd2545aef12d8e4a4f7ed969a
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-10-18 22:25:22 +0900

    feat: 커스텀 구분자 추출 기능 추가

---
+++ src/main/java/calculator/domain/CustomDelimiterExtractor.java
@@ -0,0 +1,28 @@
+package calculator.domain;
+
+public class CustomDelimiterExtractor {
+
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SUFFIX = "\n";
+
+    private CustomDelimiterExtractor() {
+    }
+
+    public static CustomDelimiterExtractor of() {
+
+        return new CustomDelimiterExtractor();
+    }
+
+    public String extractCustomDelimiter(String userInput) {
+
+        int startIndex = userInput.indexOf(CUSTOM_DELIMITER_PREFIX) + 2;
+        int endIndex = userInput.indexOf(CUSTOM_DELIMITER_SUFFIX);
+
+        return userInput.substring(startIndex, endIndex);
+    }
+
+    public boolean hasCustomDelimiterFormat(String userInput) {
+
+        return userInput.startsWith(CUSTOM_DELIMITER_PREFIX) && userInput.contains(CUSTOM_DELIMITER_SUFFIX);
+    }
+}
---
+++ src/test/java/calculator/domain/CustomDelimiterExtractorTest.java
@@ -0,0 +1,48 @@
+package calculator.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CustomDelimiterExtractorTest {
+
+    private final CustomDelimiterExtractor extractor = CustomDelimiterExtractor.of();
+
+    @Nested
+    @DisplayName("성공 케이스")
+    public class Success {
+
+        @ParameterizedTest
+        @ValueSource(strings = {"//;\n1,2,3", "//;\n"})
+        @DisplayName("커스텀 구분자를 사용하면 true를 반환한다.")
+        public void trueReturnTest(String value) {
+
+            assertThat(extractor.hasCustomDelimiterFormat(value)).isTrue();
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"1,2,3//;\n", "/;\n1,2,3"})
+        @DisplayName("올바른 커스텀 구분자 형식이 아니면 false를 반환한다.")
+        public void falseReturnTest(String value) {
+
+            assertThat(extractor.hasCustomDelimiterFormat(value)).isFalse();
+        }
+
+        @ParameterizedTest
+        @CsvSource({
+                "'//;\n1,2,3', ';'",
+                "'//!@#\n1,2','!@#'",
+                "'//:D\n1,2,3',':D'",
+                "'///;;\\\n','/;;\\'"
+        })
+        @DisplayName("커스텀 구분자를 추출하여 반환한다.")
+        public void customDelimiterExtractingTest(String value, String expected) {
+
+            assertThat(extractor.extractCustomDelimiter(value)).isEqualTo(expected);
+        }
+    }
+}
