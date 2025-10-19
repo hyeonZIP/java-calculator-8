738c46b6106bdfd10a09523debddf71961959121
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-10-19 21:58:24 +0900

    feat: 에러 메시지 객체 추가

---
+++ src/main/java/calculator/exception/ExceptionMessage.java
@@ -0,0 +1,28 @@
+package calculator.exception;
+
+public enum ExceptionMessage {
+
+    //Delimiter
+    DELIMITER_IS_NULL("[ERROR] 구분자는 null일 수 없습니다."),
+    DELIMITER_CONTAINS_DIGIT("[ERROR] 구분자는 숫자을 포함할 수 없습니다."),
+    DELIMITER_IS_BLANK("[ERROR] 구분자는 공백일 수 없습니다."),
+
+    //Number
+    NUMBER_IS_BLANK("[ERROR] 숫자 문자열은 공백일 수 없습니다."),
+    NUMBER_MUST_BE_POSITIVE("[ERROR] 숫자는 음수일 수 없습니다."),
+    NUMBER_IS_NULL("[ERROR] 슷지 믄지열은 null일 수 없습니다."),
+    NUMBER_INVALID_FORMAT("[ERROR] 숫자 형식이 올바르지 않습니다.")
+    ;
+
+    private final String message;
+
+    ExceptionMessage(String message){
+
+        this.message = message;
+    }
+
+    public String getMessage(){
+
+        return message;
+    }
+}
